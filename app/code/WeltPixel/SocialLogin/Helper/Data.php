69b845756ffc34ad47085257dfa4682bad18192c
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-12 00:51:50 +0100

    https://github.com/27estore/site/issues/53

--- app/code/WeltPixel/SocialLogin/Helper/Data.php
+++ app/code/WeltPixel/SocialLogin/Helper/Data.php
@@ -426,4 +426,16 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         return $this->_urlFactory->create();
     }
+
+    /**
+     * @return false|\Magento\Csp\Helper\CspNonceProvider
+     */
+    public function getCspNonceProvider()
+    {
+        if (class_exists(\Magento\Csp\Helper\CspNonceProvider::class)) {
+            return  \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Csp\Helper\CspNonceProvider::class);
+        }
+
+        return false;
+    }
 }
