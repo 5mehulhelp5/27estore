1d79c1973a26fa8ac17130310f707881f9ad2bcc
Dmitrii Fediuk <dfediuk@gmail.com>
2024-08-11 23:29:01 +0100

    https://github.com/27estore/site/issues/44

--- app/code/WeltPixel/ProductPage/view/frontend/web/js/changeQty.js
+++ app/code/WeltPixel/ProductPage/view/frontend/web/js/changeQty.js
@@ -9,21 +9,27 @@ define([
             this._super();
             this.qty = ko.observable($(this.qtyInput).val() * 1);
             this.maxQtyValue = $(this.qtyInput).attr('max');
+            this.lowerLimit = this.allowZeroQty ? 0 : 1;
         },
         decreaseQty: function() {
+            if ( $(this.qtyInput).attr('disabled') == 'disabled') {
+                return;
+            }
             var newQty = this.qty() - 1;
-            if (newQty < 1)
+
+            if (newQty < this.lowerLimit)
             {
-                newQty = 1;
+                newQty = this.lowerLimit;
             }
             this.qty(newQty);
         },
         increaseQty: function() {
+            if ( $(this.qtyInput).attr('disabled') == 'disabled') {
+                return;
+            }
             var newQty = this.qty() + 1;
             if (newQty <= this.maxQtyValue) {
                 this.qty(newQty);
-            } else {
-
             }
         }
     });
